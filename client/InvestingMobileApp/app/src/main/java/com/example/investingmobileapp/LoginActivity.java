c733e48e54c66409eb3a25046bb62f1c2d635b85
pvl-mr <mariyapavlova2001@mail.ru>
2021-12-12 13:42:04 +0400

    add login and registry activity front

---
+++ client/InvestingMobileApp/app/src/main/java/com/example/investingmobileapp/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.investingmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        init();
+    }
+
+    public void init() {
+        TextView noAccount = findViewById(R.id.havenotaccount);
+        noAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
---
+++ client/InvestingMobileApp/app/src/main/java/com/example/investingmobileapp/RegisterActivity.java
@@ -0,0 +1,28 @@
+package com.example.investingmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        init();
+    }
+
+    public void init() {
+        TextView haveAccount = findViewById(R.id.alreadyHaveAccount);
+        haveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
